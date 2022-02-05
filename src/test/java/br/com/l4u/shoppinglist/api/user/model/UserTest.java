2281d39ded2a8492de83fff3d6321b09cb22c04c
Bruno <brunomorello7@gmail.com>
2022-02-05 19:56:08 -0300

    Added User's missing unit test

--- src/test/java/br/com/l4u/shoppinglist/api/user/model/UserTest.java
+++ src/test/java/br/com/l4u/shoppinglist/api/user/model/UserTest.java
@@ -54,4 +54,39 @@ class UserTest {
         assertEquals(Gender.OTHERS, user.getGender(), "Gender must be equals");
         assertEquals(LocalDate.of(1996, 1, 10), user.getBirthday(), "Birthday must be equals");
     }
+
+    @Test
+    void testUserToString() {
+        user = new User();
+        user.setId("123");
+        user.setFirstName("Foo");
+        user.setLastName("Bar");
+        user.setDisplayName("Foob");
+        user.setGender(Gender.OTHERS);
+        user.setBirthday(LocalDate.of(1996, 1, 10));
+
+        assertNotNull(user.toString());
+    }
+
+    @Test
+    void testEquals() {
+        user = new User();
+        user.setId("123");
+        user.setFirstName("Foo");
+        user.setLastName("Bar");
+        user.setDisplayName("Foob");
+        user.setGender(Gender.OTHERS);
+        user.setBirthday(LocalDate.of(1996, 1, 10));
+
+        User user2 = new User();
+        user2.setId("123");
+        user2.setFirstName("Foo");
+        user2.setLastName("Bar");
+        user2.setDisplayName("Foob");
+        user2.setGender(Gender.OTHERS);
+        user2.setBirthday(LocalDate.of(1996, 1, 10));
+
+        assertTrue(user.equals(user2), "Users must be equals");
+        assertTrue(user.hashCode() == user2.hashCode(), "Hascode must be equals");
+    }
 }
